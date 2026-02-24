646bf4a3f3ddda530503f25653926467302ac69a
shreju <reachshreyas.s@gmail.com>
2026-02-24 14:26:06 +0530

    Added whitenoise configuration

--- Depth/settings.py
+++ Depth/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Depth.urls'
@@ -119,6 +120,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 static_root = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
